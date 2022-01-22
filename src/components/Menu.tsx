4ed7822adcd05dd16e1dcbf09e6fc17f3afd6fc6
Moshi <li.moshi95@gmail.com>
2022-01-22 00:15:00 -0500

    bug fix

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -4,31 +4,35 @@ import { AiOutlineMenu } from "react-icons/ai";
 import "./Menu.scss";
 
 const Menu = () => {
-  const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState<boolean | string>("");
   return (
     <React.Fragment>
       <AiOutlineMenu
-        className={`menu__btn menu__btn--${open ? "ccw" : "cw"}`}
-        onClick={() => setOpen(!open)}
+        className={`menu__btn menu__btn--${
+          open === "" ? "" : open ? "ccw" : "cw"
+        }`}
+        onClick={() => setOpen(!!!open)}
       ></AiOutlineMenu>
-      <div className={`menu menu--${open ? "open" : "close"}`}>
-        <span className="menu__link" onClick={() => setOpen(!open)}>
+      <div
+        className={`menu menu--${open === "" ? "" : open ? "open" : "close"}`}
+      >
+        <span className="menu__link" onClick={() => setOpen(!!!open)}>
           <a href="#about">
             <span>{"00. "}</span>About Me
           </a>
         </span>
 
-        <span className="menu__link" onClick={() => setOpen(!open)}>
+        <span className="menu__link" onClick={() => setOpen(!!!open)}>
           <a href="#experience">
             <span>{"01. "}</span>Experience
           </a>
         </span>
-        <span className="menu__link" onClick={() => setOpen(!open)}>
+        <span className="menu__link" onClick={() => setOpen(!!!open)}>
           <a href="#skill">
             <span>{"02. "}</span>Skills
           </a>
         </span>
-        <span className="menu__link" onClick={() => setOpen(!open)}>
+        <span className="menu__link" onClick={() => setOpen(!!!open)}>
           <a href="#contact">
             <span>{"03. "}</span>Contact
           </a>
